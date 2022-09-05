0aca1fa2a281d9ceb4ceac9d16bbf7c522775387
arturican <arturican@gmail.com>
2022-09-05 10:55:51 +0300

    Переместил и изменил компонеты Star и Rating, по уроку, так же добавил Accordion title

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,32 @@
 import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion";
+import {Rating} from "./components/Rating";
 
 function App() {
     console.log("App rendering");
     return (
         <div>
-            <TitleApp />
-            <Rating />
-            <Accordion />
-            <Rating />
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={"Список"}/>
+            <Accordion title={"Меню"}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
 
     );
 }
-function TitleApp(){
-    console.log("TitleApp rendering");
-    return <>App component</>
-}
-
-function Rating() {
-    console.log("Rating rendering");
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-
-    );
-}
-
-function Star() {
-    console.log("Star rendering");
-    return (
-        <div>star</div>
-    );
+function PageTitle(props: any){
+    console.log("PageTitle rendering");
+    return <h1>{props.title}</h1>
 }
 
 export default App;
