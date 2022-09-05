c77b088c029c003d409265afd43db669d9ae18b0
arturican <arturican@gmail.com>
2022-09-05 10:14:46 +0300

    Создал каталог component в src, создал компоненту Accordion настроил экспорт и импорт в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion";
 
 function App() {
     console.log("App rendering");
@@ -31,30 +32,6 @@ function Rating() {
         </div>
 
     );
-}
-
-function Accordion(){
-    console.log("Accordion rendering");
-    return (
-        <div>
-        <AccordionTitle />
-        <AccordionBody />
-        </div>
-);
-}
-
-function AccordionTitle(){
-    console.log("AccordionTitle rendering");
-    return <h3>Меню</h3>;
-}
-
-function AccordionBody(){
-    console.log("AccordionBody rendering");
-    return( <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>)
 }
 
 function Star() {
